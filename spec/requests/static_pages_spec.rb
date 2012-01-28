805dfe4fbc2f26ac94b98d7911dece912554478b
Dominik Boehi <dominik.boehi@gmail.com>
2012-01-28 16:13:05 +0100

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,46 +2,62 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
     
     it "should have the right title" do
      visit '/static_pages/home'
      page.should have_selector('title',
-       text: "Ruby on Rails Tutorial Sample App | Home")
+       text: "#{base_title} | Home")
     end
 
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
      visit '/static_pages/help'
      page.should have_selector('title',
-       text: "Ruby on Rails Tutorial Sample App | Help")
+       text: "#{base_title} | Help")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+        text: "#{base_title} | Contact")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About us'" do
+    it "should have the h1 'About us'" do
      visit '/static_pages/about'
-     page.should have_content('About Us')
+     page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the right title" do
      visit '/static_pages/about'
      page.should have_selector('title',
-       text: "Ruby on Rails Tutorial Sample App | About")
+       text: "#{base_title} | About")
     end
   end
 end
