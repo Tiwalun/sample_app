f83e0aa02e96b632cf19e084007daa84978db4fb
Dominik Boehi <dominik.boehi@gmail.com>
2012-01-29 11:55:06 +0100

    Change routes and remove default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  root :to => 'static_pages#home'
 
-  get "static_pages/about"
-
-  get "static_pages/contact"
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,10 +52,6 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,12 @@ describe "Static Pages" do
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', text: 'Sample App')
     end
     
     it "should have the right title" do
-     visit '/static_pages/home'
+     visit root_path
      page.should have_selector('title',
        text: "#{base_title} | Home")
     end
@@ -22,12 +22,12 @@ describe "Static Pages" do
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
-     visit '/static_pages/help'
+     visit help_path
      page.should have_selector('title',
        text: "#{base_title} | Help")
     end
@@ -36,12 +36,12 @@ describe "Static Pages" do
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
         text: "#{base_title} | Contact")
     end
@@ -50,12 +50,12 @@ describe "Static Pages" do
   describe "About page" do
 
     it "should have the h1 'About us'" do
-     visit '/static_pages/about'
+     visit about_path
      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the right title" do
-     visit '/static_pages/about'
+     visit about_path
      page.should have_selector('title',
        text: "#{base_title} | About")
     end
